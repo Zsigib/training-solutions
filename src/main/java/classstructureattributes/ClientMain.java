5e357628b3e327214a69ad8e01f2035aa28fadc0
Zsigi <zsigib@freemail.hu>
2020-11-04 19:38:36 +0100

    második videós leckék itt már a sortöést is figyelembe veszi

---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,21 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérlek add meg a neved:");
+        client.name = scanner.nextLine();
+        System.out.println("Kérlek add meg a születési éved:");
+        client.year = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Kérlek add meg a címed:");
+        client.address = scanner.nextLine();
+        System.out.println("Adataid: ");
+        System.out.println("Név: " + client.name);
+        System.out.println("Születési idő: " + client.year);
+        System.out.println("Cím: " + client.address);
+    }
+}
