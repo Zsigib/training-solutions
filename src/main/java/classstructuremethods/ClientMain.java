3dc23dc48c7e80c95436ecff92f79d9bea3dfa6e
Zsigi <zsigib@freemail.hu>
2020-11-04 20:20:38 +0100

    Harmadik videós leckék fele(Client)

---
+++ src/main/java/classstructuremethods/ClientMain.java
@@ -0,0 +1,18 @@
+package classstructuremethods;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        client.setName("Gipsz Jakab");
+        client.setYear(1969);
+        client.setAddress("Timbuktu köz 123.");
+
+        System.out.println("Név: " + client.getName());
+        System.out.println("Születési év: " + client.getYear());
+        System.out.println("Cím: " + client.getAddress());
+
+        client.migrate("Hamis utca 123.");
+        System.out.println("Új cím:" + client.getAddress());
+    }
+}
