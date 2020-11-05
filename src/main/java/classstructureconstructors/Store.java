e28707ccdb4dc282792fad66aa5cd567e3beef4b
Zsigi <zsigib@freemail.hu>
2020-11-05 06:53:20 +0100

    Konstruktorok másik fele (Store)

---
+++ src/main/java/classstructureconstructors/Store.java
@@ -0,0 +1,35 @@
+package classstructureconstructors;
+
+public class Store {
+    private String Product;
+    private int stock;
+
+    public Store(String product) {
+        this.Product = product;
+        stock = 0;
+    }
+
+    public String getProduct() {
+        return Product;
+    }
+
+    public void setProduct(String product) {
+        Product = product;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void store(int amount){
+        stock = stock + amount;
+    }
+    public void dispatch (int amount) {
+        stock = stock - amount;
+    }
+
+}
---
+++ src/main/java/classstructureconstructors/StoreMain.java
@@ -0,0 +1,21 @@
+package classstructureconstructors;
+
+public class StoreMain {
+    public static void main(String[] args) {
+        Store italbolt = new Store("Sör");
+        italbolt.store(20);
+        System.out.println(italbolt.getProduct() + "=" + italbolt.getStock());
+
+        Store perecbolt = new Store("Perec");
+        perecbolt.store(123);
+        System.out.println(perecbolt.getProduct() + "=" + perecbolt.getStock());
+
+        italbolt.store(24);
+        italbolt.dispatch(23);
+        perecbolt.store(456);
+        perecbolt.dispatch(243);
+
+        System.out.println(italbolt.getProduct() + "=" + italbolt.getStock());
+        System.out.println(perecbolt.getProduct() + "=" + perecbolt.getStock());
+            }
+}
