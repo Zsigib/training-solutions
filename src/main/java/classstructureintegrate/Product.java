7ed17d5c0ef7d4e9f783d697991dd59611054dd9
Zsigi <zsigib@freemail.hu>
2020-11-06 20:59:41 +0100

    Termék feladat működik!

--- src/main/java/classstructureintegrate/Product.java
+++ src/main/java/classstructureintegrate/Product.java
@@ -2,4 +2,34 @@ package classstructureintegrate;
 
 public class Product {
 
+    private String name;
+    private int price;
+
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int increasePrice(int change) {
+
+        return price + change;
+    }
+
+    public int decreasePrice(int change) {
+
+        return price - change;
+    }
 }
+
+
+
--- src/main/java/classstructureintegrate/ProductMain.java
+++ src/main/java/classstructureintegrate/ProductMain.java
@@ -1,7 +1,31 @@
 package classstructureintegrate;
 
+import java.util.Scanner;
+
 public class ProductMain {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Kérem a termék nevét!");
+        String name = scanner.nextLine();
+
+        System.out.println(" Kérem a termék árát!");
+        int price = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Kérem az ár változást!");
+        int change = scanner.nextInt();
+        scanner.nextLine();
+
+        Product product = new Product(name, price);
+
+        System.out.println("Eredeti ár:");
+        System.out.println(product.getName() + " " + product.getPrice());
+        System.out.println("Növelt ár:");
+        System.out.println(product.getName() + " " + product.increasePrice(change));
+        System.out.println("Csökkentett ár:");
+        System.out.println(product.getName() + " " + product.decreasePrice(change));
 
 
     }
