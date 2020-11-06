b4d37a30d2e2de108c7f23ce725b0aac3b17a407
Zsigi <zsigib@freemail.hu>
2020-11-06 10:20:40 +0100

    pénteki feladat félmegoldás

---
+++ src/main/java/week02/Language.java
@@ -0,0 +1,21 @@
+package week02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Language {
+    public static void main(String[] args) {
+
+        String[] languages = {"Java", "Python", "JavaScript"};
+        List<String> nameList = Arrays.asList(languages);
+
+        for (String length: languages) {
+
+
+        }
+
+        System.out.println();
+    }
+
+}
+
