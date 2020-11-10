27555bb314561573401179a9c2ef1f834301bc88
Zsigi <zsigib@freemail.hu>
2020-11-10 11:08:51 +0100

    órai feladat

---
+++ src/main/java/week3/Position.java
@@ -0,0 +1,38 @@
+package week3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public static void main(String[] args) {
+
+        List<Position>positions = new ArrayList<>();
+        
+        positions.add(new Position("igazgató", 2000000));
+        positions.add(new Position("középvezető", 800000));
+        positions.add(new Position("beosztott", 200000));
+
+        for (Position position: positions){
+            if (position.getBonus() > 150000) {
+                System.out.println(position);
+            }
+        }
+    }
+    }
+
