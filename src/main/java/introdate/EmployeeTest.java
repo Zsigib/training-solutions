239f5e5a542dddf3786ea620c93220e34b86b6de
Zsigi <zsigib@freemail.hu>
2020-11-10 08:24:10 +0100

    Employee feladat

---
+++ src/main/java/introdate/EmployeeTest.java
@@ -0,0 +1,13 @@
+package introdate;
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1984, 8, 15, "Gipsz Jakab" );
+
+        System.out.println(employee1.getBeginEmployment());
+        System.out.println(employee1.getDateOfBirth());
+    }
+
+
+}
