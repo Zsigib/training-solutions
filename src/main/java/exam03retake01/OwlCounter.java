e1ccfd6a85d65250fb76a9f7e6bdef6f2c004f12
Zsigi <zsigib@freemail.hu>
2021-02-22 19:27:23 +0100

    Lejárt az idő Bagolyszámlálás

--- src/main/java/exam03retake01/OwlCounter.java
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -5,14 +5,21 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 public class OwlCounter {
+
+    private Map<String, Integer>
 
     public int getNumberOfOwls(String zala) {
     return ;}
 
     public void readFromFile(BufferedReader reader) {
 
-
+        try (BufferedReader reader1 = Files.newBufferedReader(Path.of("owls.txt"))) {
+            new FileReader().readLines(reader);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
     }
 }
