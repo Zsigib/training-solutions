2ab2ba152c4ac42d478bdebec71f6eb1760e3124
Zsigi <zsigib@freemail.hu>
2021-02-22 19:28:59 +0100

    Lejárt az idő CDV

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -1,6 +1,19 @@
 package exam03retake01;
 
 public class CdvCheck {
+
     public boolean check(String s) {
+
+        try { int number = Integer.parseInt(s);
+
+        }
+        catch (IllegalArgumentException ae) {
+            throw new IllegalArgumentException("Illegal Argument Exceptiption", ae);
+
+
+
+
+
+        }
     }
 }
