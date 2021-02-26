6453335f4bff5567cfa2f92eca5718285410f453
Zsigi <zsigib@freemail.hu>
2021-02-26 20:03:10 +0100

    Harmadik vizsga 2. pótvizsga Leárt az idő

--- src/main/java/exam03retake02/Todo.java
+++ src/main/java/exam03retake02/Todo.java
@@ -1,4 +1,34 @@
 package exam03retake02;
 
 public class Todo {
+
+    private String text;
+
+    private State state;
+
+    private int priority;
+
+    public Todo(String text,  int priority) {
+        this.text = text;
+        this.state = State.NON_COMPLETED;
+        this.priority = priority;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void complete() {
+        this.state = State.COMPLETED;
+    }
+
+
 }
--- src/main/java/exam03retake02/TodoList.java
+++ src/main/java/exam03retake02/TodoList.java
@@ -1,4 +1,42 @@
 package exam03retake02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoList {
+
+    private List<Todo> todos = new ArrayList<>();
+
+    public int getNumberOfItemsLeft() {
+
+        int result = todos.size() + 1;
+
+        return result;
+    }
+
+    public <Todo> List<Todo> getMostImportantTodosText() {
+        Todo importance = (Todo) "";
+        for (int i = 0; i <todos.size(); i++)
+
+            return (List<Todo>) importance;
+    }
+
+    public void deleteCompleted() {
+
+        if Todo.
+
+
+    }
+
+
+    public List<Todo> getTodos() {
+
+        List<String> todosToFinish = new ArrayList<>();
+        for T
+
+    }
+
+    public void addTodo(Todo Todo ) {
+        todos.add();
+    }
 }
